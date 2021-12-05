ae9fcaaa24bae49cadf0d8e13d6b2ca54c7932ba
halimah0110 <halimahhhmanis@gmail.com>
2021-12-06 02:13:43 +0700

    mengatur file api client untuk penyambungan api di halaman home

--- app/src/main/java/com/example/capstoneproject/rest/ApiClient.java
+++ app/src/main/java/com/example/capstoneproject/rest/ApiClient.java
@@ -1,3 +1,22 @@
 package com.example.capstoneproject.rest;
+
+import com.example.capstoneproject.config.ServerConfig;
+
+import retrofit2.Retrofit;
+
 public class ApiClient {
+
+    private static final String BASE_URL = ServerConfig.API_ENDPOINT;
+
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient()
+    {
+        if (retrofit == null)
+        {
+            retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
+        }
+        return retrofit;
+    }
+
 }

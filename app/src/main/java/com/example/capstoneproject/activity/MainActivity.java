9977928b670d89aaa0bb193f15f31fe82e7e4db9
halimah0110 <halimahhhmanis@gmail.com>
2021-12-06 02:34:52 +0700

    mengatur penyambungan api di halaman main

--- app/src/main/java/com/example/capstoneproject/activity/MainActivity.java
+++ app/src/main/java/com/example/capstoneproject/activity/MainActivity.java
@@ -1,16 +1,80 @@
 package com.example.capstoneproject.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.capstoneproject.DBHelper;
 import com.example.capstoneproject.R;
+import com.example.capstoneproject.adapter.RecyclerMoviesAdapter;
+import com.example.capstoneproject.model.Response;
+import com.example.capstoneproject.model.Result;
+import com.example.capstoneproject.rest.ApiClient;
+import com.example.capstoneproject.rest.ApiInterface;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerMoviesAdapter adapter;
+
+    private TextView username;
+    String API_KEY = "dd7c30cbeb58edd0347801dfc5453354";
+    String LANGUAGE = "en-US";
+    String CATEGORY = "popular";
+    int PAGE = 1;
+    RecyclerView recyclerview;
+    ImageView notifikasi;
+    DBHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+        recyclerview = findViewById(R.id.moviePopular);
+        recyclerview.setHasFixedSize(true);
+        recyclerview.setLayoutManager(new LinearLayoutManager(this));
+
+        username = findViewById(R.id.username);
+        username.getText();
+
+
+
+
+        panggilRetrofit();
+
     }
+
+    private void panggilRetrofit() {
+        ApiInterface apiInterface = ApiClient.getClient().create(ApiInterface.class);
+        Call<Response> call = apiInterface.getMovie(CATEGORY, API_KEY, LANGUAGE, PAGE);
+        call.enqueue(new Callback<Response>() {
+            @Override
+            public void onResponse(Call<Response> call, retrofit2.Response<Response> response) {
+                List<Result> mList = response.body().getResults();
+                adapter = new RecyclerMoviesAdapter(MainActivity.this, mList);
+                recyclerview.setAdapter(adapter);
+            }
+
+            @Override
+            public void onFailure(Call<Response> call, Throwable t) {
+                t.fillInStackTrace();
+            }
+        });
+    }
+
+
 }
--- app/src/main/java/com/example/capstoneproject/rest/ApiClient.java
+++ app/src/main/java/com/example/capstoneproject/rest/ApiClient.java
@@ -3,6 +3,7 @@ package com.example.capstoneproject.rest;
 import com.example.capstoneproject.config.ServerConfig;
 
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 
